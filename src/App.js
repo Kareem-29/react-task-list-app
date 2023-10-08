9d9e6cb937b040f411c588f0fe8f31fb9641e412
Kareem-29 <101586124+Kareem-29@users.noreply.github.com>
2023-10-08 17:16:42 +0300

    Files Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component , useEffect, useState} from "react"; //to create class component, and also for babel to use jsx
+import "./App.css"
 
-function App() {
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks || []);
+  const [taskName, setTaskName] = useState('');
+
+  const taskList = tasks.map((task) => (
+    <ul role="list" id="myUl">
+    <div className="list-items">
+      <li>{task.name}</li>
+    </div>
+    </ul>
+  ));
+
+  const NewTask = () => {
+    if (taskName.trim() !== '') {
+      const newTask = {
+        
+        name: taskName,
+      };
+
+      setTasks([...tasks, newTask]);
+      setTaskName(''); // Clear the input 
+    } else {
+      alert('Please enter a task name.');
+    }
+  };
+
+  const ClearList = () => {
+    setTasks([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>My List</h1>
+      <button onClick={ClearList}>Clear List</button>
+      <ul role="list" id="myUl">
+        <div className="list-items" id="newli">
+          {taskList}
+        </div>
+      </ul>
+
+      <div>
+        <input
+          type="text"
+          id="newTask"
+          className="input input__lg"
+          placeholder="Type your text here"
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
+        />
+        <button onClick={NewTask} className="btn btn__primary btn__lg">
+          Add item
+        </button>
+      </div>
     </div>
   );
 }
+
+
+
 
 export default App;
